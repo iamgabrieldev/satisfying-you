d162bc5e310499b4853541f215e2cf50a76210a6
jguih <gui.kcoal+github@hotmail.com>
2024-04-05 23:50:35 -0300

    Include TextField States

--- src/components/forms/TextField.tsx
+++ src/components/forms/TextField.tsx
@@ -1,15 +1,25 @@
 import { forwardRef } from "react";
-import { TextInput as NativeTextInput, View } from "react-native";
+import {
+  NativeSyntheticEvent,
+  TextInput as NativeTextInput,
+  TextInputChangeEventData,
+  View,
+} from "react-native";
 import { HelperText, TextInput } from "react-native-paper";
 
 export type TextFieldProps = {
   label?: string;
   helperText?: string;
   isError?: boolean;
+  value?: string;
+  onChange?: (e: NativeSyntheticEvent<TextInputChangeEventData>) => void;
 };
 
 export const TextField = forwardRef<NativeTextInput, TextFieldProps>(
-  function TextField({ label, helperText, isError }, forwardedRef) {
+  function TextField(
+    { label, helperText, isError, value, onChange },
+    forwardedRef,
+  ) {
     return (
       <View style={{ gap: 1 }}>
         <TextInput
@@ -17,6 +27,8 @@ export const TextField = forwardRef<NativeTextInput, TextFieldProps>(
           mode="flat"
           error={isError}
           ref={forwardedRef}
+          value={value}
+          onChange={onChange}
         />
         {helperText && (
           <HelperText visible={isError} type="error">
